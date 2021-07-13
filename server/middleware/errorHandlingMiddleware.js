9bce2790ed38ef9416bbcdbcc8eb3e23ad178ead
Evgeniy Bulbenkov <EvgeniyBulbenkov@gmail.com>
2021-07-14 02:16:26 +0300

    Fixed function invocation argument's list

--- server/middleware/errorHandlingMiddleware.js
+++ server/middleware/errorHandlingMiddleware.js
@@ -1,6 +1,6 @@
 const ApiError = require('../error/apiError')
 
-module.exports = (err, req, res) => {
+module.exports = (err, req, res, next) => {
     if (err instanceof ApiError) {
         return res.status(err.status).json({message: err.message});
     }
