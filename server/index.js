527437d7f7f34fe677268d35207a5bb14da9b912
Evgeniy Bulbenkov <EvgeniyBulbenkov@gmail.com>
2021-07-13 20:13:57 +0300

    Added model defenition, added models in index.js

--- server/index.js
+++ server/index.js
@@ -1,10 +1,18 @@
 require('dotenv').config();
 const express = require('express');
-const sequelize = require('./db')
+const sequelize = require('./db');
+const models = require('./models/models');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.status(200).json({message: 'Hello World!!!'})
+})
 
 const start = async () => {
     try {
