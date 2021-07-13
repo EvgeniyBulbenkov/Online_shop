f23c5f3fc096a388c966736701240112d5e0556e
Evgeniy Bulbenkov <EvgeniyBulbenkov@gmail.com>
2021-07-14 02:03:55 +0300

    Connected routing and error handling middleware, added date/time to the startup message

--- server/index.js
+++ server/index.js
@@ -3,12 +3,16 @@ const express = require('express');
 const sequelize = require('./db');
 const models = require('./models/models');
 const cors = require('cors');
+const router = require('./routes');
+const errorHandler = require('./middleware/errorHandlingMiddleware');
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('/api', router);
+app.use(errorHandler)
 
 app.get('/', (req, res) => {
     res.status(200).json({message: 'Hello World!!!'})
@@ -18,7 +22,8 @@ const start = async () => {
     try {
         await sequelize.authenticate();
         await sequelize.sync();
-        app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+        let time = new Date(Date.now()).toISOString();
+        app.listen(PORT, () => console.log(time, `: Server started on port ${PORT}`));
     } catch (e) {
         console.log(e);
     }
